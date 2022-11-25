81e0d31b0eaa33468c8afe05aef074ee33b89e7c
NMS1010 <nguyenminhson102002@gmail.com>
2022-11-25 19:12:49 +0700

    add interface for falcuty service + repository

--- src/main/java/models/repositories/faculty/IFacultyRepository.java
+++ src/main/java/models/repositories/faculty/IFacultyRepository.java
@@ -1,4 +1,12 @@
 package models.repositories.faculty;
 
-public interface IFacultyRepository {
+import common.interfaces.IModifyEntity;
+import common.interfaces.IRetrieveEntity;
+import models.view_models.faculty.FacultyCreateRequest;
+import models.view_models.faculty.FacultyUpdateRequest;
+import models.view_models.faculty.FacultyViewModel;
+
+public interface IFacultyRepository extends IModifyEntity<FacultyCreateRequest, FacultyUpdateRequest, String, String>,
+        IRetrieveEntity<FacultyViewModel, String, String> {
+
 }
--- src/main/java/models/services/faculty/FacultyService.java
+++ src/main/java/models/services/faculty/FacultyService.java
@@ -1,4 +1,34 @@
 package models.services.faculty;
 
-public class FacultyService {
+import models.view_models.faculty.FacultyCreateRequest;
+import models.view_models.faculty.FacultyUpdateRequest;
+import models.view_models.faculty.FacultyViewModel;
+
+import java.util.ArrayList;
+
+public class FacultyService implements IFacultyService{
+    @Override
+    public boolean insert(FacultyCreateRequest request) {
+        return false;
+    }
+
+    @Override
+    public boolean update(FacultyUpdateRequest request) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String hashKey, String rangeKey) {
+        return false;
+    }
+
+    @Override
+    public FacultyViewModel retrieveById(String hashKey, String rangeKey) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<FacultyViewModel> retrieveAll() {
+        return null;
+    }
 }
