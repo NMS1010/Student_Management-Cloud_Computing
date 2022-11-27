5bcd1b6ab6f4441582ec2c9daa55d4ddb99c2934
NMS1010 <nguyenminhson102002@gmail.com>
2022-11-27 22:54:41 +0700

    modify class name

--- src/main/java/models/repositories/student/IStudentRepository.java
+++ src/main/java/models/repositories/student/IStudentRepository.java
@@ -2,11 +2,11 @@ package models.repositories.student;
 
 import common.interfaces.IModifyEntity;
 import common.interfaces.IRetrieveEntity;
-import models.view_models.student.StudentClassCreateRequest;
-import models.view_models.student.StudentClassUpdateRequest;
+import models.view_models.student.StudentCreateRequest;
+import models.view_models.student.StudentUpdateRequest;
 import models.view_models.student.StudentViewModel;
 
-public interface IStudentRepository extends IModifyEntity<StudentClassCreateRequest, StudentClassUpdateRequest, String, String>,
+public interface IStudentRepository extends IModifyEntity<StudentCreateRequest, StudentUpdateRequest, String, String>,
         IRetrieveEntity<StudentViewModel, String, String> {
     boolean containStudentClass(String studentClassId);
 }
--- src/main/java/models/services/student/IStudentService.java
+++ src/main/java/models/services/student/IStudentService.java
@@ -1,14 +1,14 @@
 package models.services.student;
 
-import models.view_models.student.StudentClassCreateRequest;
-import models.view_models.student.StudentClassUpdateRequest;
+import models.view_models.student.StudentCreateRequest;
+import models.view_models.student.StudentUpdateRequest;
 import models.view_models.student.StudentViewModel;
 
 import java.util.ArrayList;
 
 public interface IStudentService {
-    boolean insert(StudentClassCreateRequest request);
-    boolean update(StudentClassUpdateRequest request);
+    boolean insert(StudentCreateRequest request);
+    boolean update(StudentUpdateRequest request);
     boolean delete(String hashKey, String rangeKey);
     StudentViewModel retrieveById(String hashKey, String rangeKey);
     ArrayList<StudentViewModel> retrieveAll();
